d0fd41631e6912c17bdcb5595562eced31343c7b
akash chavda <info.jvmarketingsolution@gmail.com>
2026-05-10 22:30:31 +0530

    Cleanup: Deleted redundant scripts, logs, zip backups, and conflict files to free up disk space

--- check.js
+++
@@ -1,9 +0,0 @@
-const searchDigits = "99";
-const regexPattern = searchDigits.split('').join('[\\s\\-()]*');
-console.log(regexPattern);
-try {
-  new RegExp(regexPattern);
-  console.log("SUCCESS");
-} catch(e) {
-  console.error("ERROR", e.message);
-}
